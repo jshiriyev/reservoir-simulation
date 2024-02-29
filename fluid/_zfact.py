227575a5279613f9b71a480a12ce2ed98f4f8008
Javid Shiriyev <jshiriyev@utexas.edu>
2024-02-29 23:25:40 +0400

    successfully calculated z factor

--- fluid/_zfact.py
+++ fluid/_zfact.py
@@ -1,35 +1,39 @@
 import math
 
-"""
-Hall-Yarborough
-Dranchuk-Abu-Kassem
-Dranchuk-Purvis-Robinson
-"""
+import numpy
 
 from scipy import optimize
 
-def unknown(Pr,Tr):
+def unknown(Pr:numpy.ndarray,Tr:float):
 	"""Function to Calculate Gas Compressibility Factor
+
 	Pr	: reduced pressure
 	Tr	: reduced temperatue
 	"""
+	Pr = numpy.asarray(Pr)
 
 	a = 1.39*(Tr-0.92)**0.5-0.36*Tr-0.101
 	b = (0.62-0.23*Tr)*Pr+(0.066/(Tr-0.86)-0.037)*Pr**2+0.32*Pr**6/(10**(9*(Tr-1)))
-	c = (0.132-0.32*math.log(Tr)/math.log(10))
+	c = (0.132-0.32*numpy.log(Tr)/numpy.log(10))
 	d = 10**(0.3106-0.49*Tr+0.1824*Tr**2)
 
-	return a+(1-a)*math.exp(-b)+c*Pr**d
+	return a+(1-a)*numpy.exp(-b)+c*Pr**d
+
+HY = """
+Hall Yarborough method is not recommended for application\n\
+if pseudo-reduced temperature is less than one.
+"""
 
 def Hall_Yarborough(Pr,Tr):
 	"""
+	Hall-Yarborough Method
+
 	Pr	: reduced pressure
 	Tr	: reduced temperatue
 	"""
 
 	if Tr<1:
-		raise Warning("Hall Yarborough method is not recommended for \
-			application if pseudo-reduced temperature is less than one.")
+		raise Warning(HY)
 
 	X1 = -0.06125*Pr/Tr*math.exp(-1.2*(1-1/Tr)**2)
 	X2 = 14.76/Tr-9.76/Tr**2+4.58/Tr**3
@@ -45,55 +49,70 @@ def Hall_Yarborough(Pr,Tr):
 
 	return 0.06125*Pr/Tr/Y*math.exp(-1.2*(1-1/Tr)**2)
 
+DAK = """
+Dranchuk-Abu-Kassem method represents the Standing-Katz correlation:\n\
+0.2 < Pr < 15 and 0.7 < Tr < 3.0 within 1 % error \n\
+ 15 < Pr < 30 within 3% error.
+"""
+
 def Dranchuk_Abu_Kassem(Pr,Tr):
 	"""
+	Dranchuk-Abu-Kassem Method
+
 	Pr	: reduced pressure
 	Tr	: reduced temperatue
 	"""
 
-	if Tr<1 or Tr>3 or Pr<0.2 or Pr>30:
-		raise Warning("Dranchuk-Abu-Kassem method is recommended for \
-			applications where 0.2 < Pr < 30 and 1.0 < Tr < 3.0.")
-
-	A1  = 0.3265
+	if Pr>30:
+		raise Warning(DAK)
+	elif Pr<0.2:
+		raise Warning(DAK)
+	elif Pr<15 and (Tr<0.7 or Tr>3.0):
+		raise Warning(DAK)
+		
+	A1  =  0.3265
 	A2  = -1.0700
 	A3  = -0.5339
-	A4  = 0.01569
+	A4  =  0.01569
 	A5  = -0.05165
-	A6  = 0.5475
+	A6  =  0.5475
 	A7  = -0.7361
-	A8  = 0.1844
-	A9  = 0.1056
-	A10 = 0.6134
-	A11 = 0.7210
+	A8  =  0.1844
+	A9  =  0.1056
+	A10 =  0.6134
+	A11 =  0.7210
 
 	R1 = A1+A2/Tr+A3/Tr**3+A4/Tr**4+A5/Tr**5
-	R2 = 0.27*(Pr/Tr)
-	R3 = A6+A7/Tr+A8/Tr**2
-	R4 = A9*(A7/Tr+A8/Tr**2)
-	R5 = A10/Tr**3
+	R2 = A6+A7/Tr+A8/Tr**2
+	R3 = A9*(A7/Tr+A8/Tr**2)
+	R4 = A10/Tr**3
+	R5 = (0.27*Pr)/Tr
 
-	FYF = lambda rhor: 1+R1*rhor-R2/rhor+R3*rhor**2\
-			-R4*rhor**5+R5*(1+A11*rhor**2)*math.exp(-A11*rhor**2)
+	FYF = lambda rhor: 1+R1*rhor+R2*rhor**2-R3*rhor**5\
+			+R4*(1+A11*rhor**2)*rhor**2*math.exp(-A11*rhor**2)-R5/rhor
 
-	FYP = lambda rhor: R1+R2/rhor**2+2*R3*rhor-5*R4*rhor**4\
-			+2*R5*rhor*math.exp(-A11*rhor**2)*(1+2*A11*rhor**3-A11*rhor**2*(1+A11*rhor**2))
+	FYP = lambda rhor: R1+2*R2*rhor-5*R3*rhor**4\
+			+2*R4*rhor*(1+A11*rhor**2*(1-A11*rhor**2))*math.exp(-A11*rhor**2)+R5/rhor**2
 
-	rhor0 = 0.27*(Pr/Tr)
+	rhor = optimize.newton(FYF,R5,fprime=FYP)
 
-	rhor = optimize.newton(FYF,rhor0,fprime=FYP)
+	return (0.27*Pr)/(rhor*Tr)
 
-	return 0.27*(Pr)/(rhor*Tr)
+DPR = """
+Dranchuk-Purvis-Robinson method is recommended for applications where\n\
+0.2 < Pr < 3.0 and 1.05 < Tr < 3.0.
+"""
 
 def Dranchuk_Purvis_Robinson(Pr,Tr):
 	"""
+	Dranchuk-Purvis-Robinson Method
+
 	Pr	: reduced pressure
 	Tr	: reduced temperatue
 	"""
 
 	if Tr<1.05 or Tr>3 or Pr<0.2 or Pr>3.0:
-		raise Warning("Dranchuk-Purvis-Robinson method is recommended for \
-			applications where 0.2 < Pr < 3.0 and 1.05 < Tr < 3.0.")
+		raise Warning(DPR)
 
 	A1 =  0.31506237
 	A2 = -1.0467099
@@ -108,22 +127,49 @@ def Dranchuk_Purvis_Robinson(Pr,Tr):
 	T2 = A4+A5/Tr
 	T3 = A5*A6/Tr
 	T4 = A7/Tr**3
-	T5 = 0.27*Pr/Tr
+	T5 = (0.27*Pr)/Tr
 
 	FYF = lambda rhor: 1+T1*rhor+T2*rhor**2+T3*rhor**5\
 		+(T4*rhor**2*(1+A8*rhor**2)*math.exp(-A8*rhor**2))-T5/rhor
 
-	rhor0 = 0.27*(Pr/Tr)
+	rhor = optimize.newton(FYF,T5)
 
-	rhor = optimize.newton(FYF,rhor0)
-
-	return 0.27*(Pr)/(rhor*Tr)
+	return (0.27*Pr)/(rhor*Tr)
 
 if __name__=="__main__":
+
+	import matplotlib.pyplot as plt
+
+	import numpy as np
 
 	Pr,Tr = 2.99,1.52
 
 	print(unknown(Pr,Tr))
-	print(Hall_Yarborough(Pr,Tr))
+	# print(Hall_Yarborough(Pr,Tr))
 	print(Dranchuk_Abu_Kassem(Pr,Tr))
-	print(Dranchuk_Purvis_Robinson(Pr,Tr))
+	# print(Dranchuk_Purvis_Robinson(Pr,Tr))
+
+	Pr = np.linspace(0.2,8,200)
+
+	z1 = unknown(Pr,Tr)
+	# z2 = np.zeros(Pr.shape)
+	z3 = np.zeros(Pr.shape)
+	# z4 = np.zeros(Pr.shape)
+
+	for i,P in enumerate(Pr):
+		# z2[i] = Hall_Yarborough(P,Tr)
+		z3[i] = Dranchuk_Abu_Kassem(P,Tr)
+		# z4[i] = Dranchuk_Purvis_Robinson(P,Tr)
+
+	plt.plot(Pr,z1,label = "unknown")
+	# plt.plot(Pr,z2,label = "Hall_Yarborough")
+	plt.plot(Pr,z3,label = "Dranchuk_Abu_Kassem")
+	# plt.plot(Pr,z4,label = "Dranchuk_Purvis_Robinson")
+
+	plt.legend()
+
+	plt.show()
+
+
+
+	
