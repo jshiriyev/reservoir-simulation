8e5fb044166f17b81d844b65e8357544f4281bfd
Javid Shiriyev, Ph.D <jshiriyev@utexas.edu>
2024-03-07 15:25:17 +0400

    tried to add some oil equations

--- respy/fluid/_oil.py
+++ respy/fluid/_oil.py
@@ -52,3 +52,74 @@ class Oil():
     def get_viscosity(self):
         
         pass
+
+def Rst():
+    """Calculates the stock-tank GOR. It should not be used
+    if the separator temperature is >140F."""
+
+    A1 =  0.3818
+    A2 = -5.506
+    A3 =  2.902
+    A4 =  1.327
+    A5 = -0.7355
+
+    logrst = A1+A2*numpy.log(gamma_oil)+A3*numpy.log(gamma_gas_sp)+\
+                A4*numpy.log(Psp)+A5*numpy.log(Tsp)
+
+    return numpy.exp(logrst)
+
+def pbubble(temp):
+    """
+    Calculates the bubblepoint pressure of the oil at reservoir conditions
+
+    Rs          : solution GOR including stock-tank vent gas
+    gamma_gas   : specific gravity of the separator gas
+
+    The equations are valid to 325F.
+    """
+
+    Cpb = (Rs/gamma_gas)**0.83*10**(0.00091*temp-0.0125*gamma_API)
+
+    return 18.2*(Cpb-1.4)
+
+def gor(pres,temp):
+    """
+    Calculates the solution GOR
+
+    pres        : pressure values below bubble point pressure
+    temp        : temperature
+    gamma_gas   : specific gravity of the separator gas
+    """
+
+    Cpb = pres/18.2+1.4
+
+    return (Cpb*10**(-0.00091*temp+0.0125*gamma_API))**(1/0.83)*gamma_gas
+
+def fvf(pres,temp,Rs,gamma_gas,gamma_oil):
+
+    CBob = Rs*(gamma_gas/gamma_oil)**0.5+1.25*temp
+
+    Bob = 0.9759+12e-5*CBob**1.2
+
+    return Bob*numpy.exp(co*(pbubble-pres))
+
+def rho(pres):
+
+    rhor = (rhoSTO+0.01357*Rs*gamma_gas)/fvf
+
+    return rhob*numpy.exp(comp*(pres-pbubble))
+
+def comp():
+
+    A1 =  -1_433.0
+    A2 =       5.0
+    A3 =      17.2
+    A4 =  -1_180.0
+    A5 =      12.61
+    A6 = 100_000.0
+
+    return (A1+A2*Rs+A3*temp+A4*gamma_gas+A5*gamma_API)/(A6*pres)
+
+def visc():
+
+    pass
