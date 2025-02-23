2ecc14b0deb8dbd93c27474d129d7b9706847b20
Javid Shiriyev, Ph.D. <jshiriyev@utexas.edu>
2025-02-23 17:41:04 +0400

    Well is well prepared.

--- respy/__init__.py
+++ respy/__init__.py
@@ -1,17 +1,26 @@
+# Analytical Solution
 # from . import linear
 # from . import radial
 # from . import green
 
+# 1. Input Data
+
+# 2. Gridding
 from .grids import Grids
 from .grids import GridDelta
 from .grids import GridRegular
 
+# 3. Property Calculation
 # from . import fluid_props
-
 # from .fluid_props import phaseg
-
 # from . import cappres
 # from . import relperm
 # from . import fluid
 
+# 4. Configuration
+from .configure import Well
+
+# 5. Simulation
 # from . import solver
+
+# 6. Post-Processing
--- respy/configure/__init__.py
+++ respy/configure/__init__.py
@@ -1,1 +1,8 @@
 # preprocessing folder
+
+# from .constraints._edge import Edge
+from .constraints._well import Well
+
+# from .constraints._locator import Locator
+
+# from .constraints._schedule import Schedule
