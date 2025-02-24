00dc2022aeea4ea2f619569a8c89b46a21b6aa2e
Javid Shiriyev, Ph.D. <jshiriyev@utexas.edu>
2025-02-24 15:34:00 +0400

    .

--- respy/simulation/_block.py
+++ respy/simulation/_block.py
@@ -1,169 +1,42 @@
 import numpy
 
+from ._cuboid import Cuboid
 from ._vector import Vector
 
-class Block():
+class Block(Cuboid):
 
-    def __init__(self,grids,rock=None,fluid=None):
+    def __init__(self,grids,rrock=None,fluid=None,tcomp=None):
         """Initialization of block (cell) calculation class."""
-        self.grids = grids
-        self.rock  = rock
-        self.fluid = fluid
+        super().__init__(grids,rrock,fluid,tcomp)
 
-    def __getattr__(self,key):
-        """Delegates attribute access to the underlying grid object."""
-        return getattr(self.grids,key)
+    def vector(self,tstep=1.,wells=None,edges=None):
+        """Returns vector to be used in the building."""
+        avect = self.accumulation(tstep) # accumulation vector
+        tvect = self.transmissibility() # inter-block transmissibility vector
+        wvect = self.boreholes(wells) # wells' block productivity vector list
+        evect = self.boundaries(edges) # edges' block productivity vector list
 
-    def __call__(self,rock=None,fluid=None,wells=None,edges=None,tstep=1.):
-        """Returns pressure updated Vector instance."""
-
-        self.rock  = rock # permeability and transmissibility
-        self.fluid = fluid # hydrostatic head, potential and mobility
-
-        # accumulation & inter-block transmissibility
-        tvect = self.cell_vector(tstep)
-
-        # well block productivity
-        wvect = self.well_vector(wells)
-
-        # boundary block transmissibility
-        bvect = self.edge_vector(edges)
-
-        return Vector(*tvect,wvect,bvect)
-
-    @rock.setter
-    def rock(self,rock):
-        """Sets reservoir rock permeability and transmissibility into the cell."""
-        if rock is None:
-            return
-
-        self.xflow = rock
-        self.yflow = rock
-        self.zflow = rock
-
-    @fluid.setter
-    def fluid(self,fluid):
-        """Sets fluid potential and mobility into the cell."""
-        if fluid is None:
-            return
-            
-        self.hhead = fluid
-        self.power = fluid
-        self.mobil = fluid
-
-    @xflow.setter
-    def xflow(self,rock):
-        """Setter for the rock transmissibility in x-direction."""
-        self._xflow = (rock._xperm*self._xarea)/(self._xdelta)
-
-    @yflow.setter
-    def yflow(self,rock):
-        """Setter for the rock transmissibility in y-direction."""
-        self._yflow = (rock._yperm*self._yarea)/(self._ydelta)
-
-    @zflow.setter
-    def zflow(self,rock):
-        """Setter for the rock transmissibility in z-direction."""
-        self._zflow = (rock._zperm*self._zarea)/(self._zdelta)
-
-    @hhead.setter
-    def hhead(self,fluid):
-        """Setter for the fluid's hydrostatic head."""
-        self._hhead = fluid._grad*self._depth
-
-    @power.setter
-    def power(self,fluid):
-        """Setter for the fluid's phase potential."""
-        self._power = fluid._press+self._hhead
-
-    @mobil.setter
-    def mobil(self,fluid):
-        """Setter for the fluid's phase mobility."""
-        self._mobil = (fluid._rperm)/(fluid._visc*fluid._fvf)
-
-    def cell_vector(self,tstep):
-        """Returns A.ct, Tx, Ty, and Tz in the form of flat arrays."""
-
-        acvect = self.accumulation(tstep)
-
-        xneg,xpos = self.xneg,self.xpos
-        yneg,ypos = self.yneg,self.ypos
-        zneg,zpos = self.zneg,self.zpos
-
-        xrmean = self.mean_harmonic(xneg.xflow,xpos.xflow)
-        yrmean = self.mean_harmonic(yneg.yflow,ypos.yflow)
-        zrmean = self.mean_harmonic(zneg.zflow,zpos.zflow)
-
-        xfmean = self.upwinding(
-            xneg.mobil,xpos.mobil,xneg.power,xpos.power)
-
-        yfmean = self.upwinding(
-            yneg.mobil,ypos.mobil,yneg.power,ypos.power)
-
-        zfmean = self.upwinding(
-            zneg.mobil,zpos.mobil,zneg.power,zpos.power)
-
-        return (acvect,xrmean*xfmean,yrmean*yfmean,zrmean*zfmean,self.hhead)
+        return Vector(avect,*tvect,wvect,evect)
 
     def accumulation(self,tstep:float):
         """Returns accumulation multiplied compressibility (A.ct)."""
-        return (self.volume*self.rock.poro)/(tstep*86400)
+        return (self.volume*self.rrock.poro*self.tcomp)/tstep
 
-    @property
-    def compressibility(self):
-        """Returns total compressibility."""
-        if "tcomp" in self.prop:
-            return self.tcomp
-        return self.rcomp+self.fcomp
+    def transmissibility(self):
+        """Returns Tx, Ty, and Tz in the form of flat arrays."""
+        tx = self.flux(self.xflow,self.xnegbool,self.xposbool)
+        ty = self.flux(self.yflow,self.ynegbool,self.yposbool)
+        tz = self.flux(self.zflow,self.znegbool,self.zposbool)
 
-    def well_vector(self,wells):
-        """Returns productivity for all active wells."""
-        wells = () if wells is None else wells
-        return [self.well_productivity(well) for well in wells]
+        return tx,ty,tz
 
-    def edge_vector(self,edges):
-        """Returns transmissibility for all active boundaries."""
-        edges = () if edges is None else edges
-        return [self.edge_productivity(edge) for edge in edges]
+    def flux(self,flow,negbool,posbool):
+        """Helper function to compute mean harmonic and upwinded mobility product."""
+        rmean = self.mean_harmonic(flow[negbool],flow[posbool])
+        fmean = self.upwinding(
+            self.mobil[negbool],self.mobil[posbool],self.power[negbool],self.power[posbool])
 
-    def well_productivity(self,well):
-        """Returns well transmissibility values for the given well condition."""
-
-        if well.axis=="x":
-            k1 = self.rock.yperm[list(well.block)]
-            k2 = self.rock.zperm[list(well.block)]
-            w1 = self.grids.ydelta[list(well.block)]
-            w2 = self.grids.zdelta[list(well.block)]
-            w3 = self.grids.xdelta[list(well.block)]
-        elif well.axis=='y':
-            k1 = self.rock.xperm[list(well.block)]
-            k2 = self.rock.zperm[list(well.block)]
-            w1 = self.grids.xdelta[list(well.block)]
-            w2 = self.grids.zdelta[list(well.block)]
-            w3 = self.grids.ydelta[list(well.block)]
-        elif well.axis=='z':
-            k1 = self.rock.xperm[list(well.block)]
-            k2 = self.rock.yperm[list(well.block)]
-            w1 = self.grids.xdelta[list(well.block)]
-            w2 = self.grids.ydelta[list(well.block)]
-            w3 = self.grids.zdelta[list(well.block)]
-
-        well.prod = (2*numpy.pi*w3*numpy.sqrt(k1*k2))/(
-            numpy.log(self.requivalent(k1,k2,w1,w2)/well.radius)+well.skin
-            )*self.mobil[list(well.block)]
-        
-        return well
-
-    def edge_productivity(self,edge):
-        """Returns exterior block transmissibility values for the
-        given boundary condition."""
-
-        face = getattr(self,edge.face)
-        flow = getattr(face,f"{edge.axis}flow")
-
-        edge._prod = flow*face.mobil
-
-        return edge
+        return rmean*fmean
 
     @staticmethod
     def mean_weighted(term1,term2):
@@ -185,6 +58,39 @@ class Block():
         """Computes the upwinded mean of two terms based on phase potential (ppot) values."""
         return numpy.where(ppot1<ppot2,term2,term1)
 
+    def boreholes(self,wells):
+        """Returns productivity for all active wells."""
+        wells = () if wells is None else wells
+        return [self.well_productivity(well) for well in wells]
+
+    def well_productivity(self,well):
+        """Returns well transmissibility values for the given well condition."""
+
+        if well.axis=="x":
+            k1 = self.rrock.yperm[list(well.block)]
+            k2 = self.rrock.zperm[list(well.block)]
+            w1 = self.grids.ydelta[list(well.block)]
+            w2 = self.grids.zdelta[list(well.block)]
+            w3 = self.grids.xdelta[list(well.block)]
+        elif well.axis=='y':
+            k1 = self.rrock.xperm[list(well.block)]
+            k2 = self.rrock.zperm[list(well.block)]
+            w1 = self.grids.xdelta[list(well.block)]
+            w2 = self.grids.zdelta[list(well.block)]
+            w3 = self.grids.ydelta[list(well.block)]
+        elif well.axis=='z':
+            k1 = self.rrock.xperm[list(well.block)]
+            k2 = self.rrock.yperm[list(well.block)]
+            w1 = self.grids.xdelta[list(well.block)]
+            w2 = self.grids.ydelta[list(well.block)]
+            w3 = self.grids.zdelta[list(well.block)]
+
+        well.prod = (2*numpy.pi*w3*numpy.sqrt(k1*k2))/(
+            numpy.log(self.requivalent(k1,k2,w1,w2)/well.radius)+well.skin
+            )*self.mobil[list(well.block)]
+        
+        return well
+
     @staticmethod
     def requivalent(perm1,perm2,delta1,delta2):
         """Returns equivalent radius of grids that contains well."""
@@ -195,6 +101,22 @@ class Block():
         quar12 = numpy.power(perm1/perm2,1/4)
 
         return 0.28*numpy.sqrt(sqrt21+sqrt12)/(quar21+quar12)
+
+    def boundaries(self,edges):
+        """Returns transmissibility for all active boundaries."""
+        edges = () if edges is None else edges
+        return [self.edge_productivity(edge) for edge in edges]
+
+    def edge_productivity(self,edge):
+        """Returns exterior block transmissibility values for the
+        given boundary condition."""
+
+        face = getattr(self,edge.face)
+        flow = getattr(face,f"{edge.axis}flow")
+
+        edge._prod = flow*face.mobil
+
+        return edge
 
 if __name__ == "__main__":
 
---
+++ respy/simulation/_cuboid.py
@@ -0,0 +1,133 @@
+import numpy
+
+class Cuboid():
+    """Rectangular Cuboid class"""
+
+    def __init__(self,grids,rrock=None,fluid=None,tcomp=None):
+        """Initialization of block (cell) calculation class."""
+        self.grids = grids
+        self.rrock = rrock
+        self.fluid = fluid
+        self.tcomp = tcomp
+
+    def __getattr__(self,key):
+        """Delegates attribute access to the underlying grid object."""
+        return getattr(self.grids,key)
+
+    def __call__(self,rrock=None,fluid=None,tcomp=None):
+        """Returns the instance with updated rock and fluid properties."""
+        self.rrock = rrock # reservoir rock properties
+        self.fluid = fluid # reservoir fluid properties
+        self.tcomp = tcomp # total compressibility
+
+        return self
+
+    @property
+    def rrock(self):
+        """Getter for the reservoir rock properties."""
+        return self._rrock
+    
+    @rrock.setter
+    def rrock(self,value):
+        """Sets the reservoir rock permeability and transmissibility into the cell."""
+        if value is None:
+            return
+
+        self._rrock = value
+
+        # Reset flow properties to indicate they need recalculation
+        self.xflow = None # Block transmissibility in x-direction
+        self.yflow = None # Block transmissibility in y-direction
+        self.zflow = None # Block transmissibility in z-direction
+
+    @property
+    def xflow(self):
+        """Getter for the rock transmissibility in x-direction."""
+        return self._xflow/(9.869233e-16*0.3048)
+    
+    @xflow.setter
+    def xflow(self,value):
+        """Setter for the rock transmissibility in x-direction."""
+        self._xflow = (self.rrock._xperm*self._xarea)/(self._xdelta)
+
+    @property
+    def yflow(self):
+        """Getter for the rock transmissibility in y-direction."""
+        return self._yflow/(9.869233e-16*0.3048)
+
+    @yflow.setter
+    def yflow(self,value):
+        """Setter for the rock transmissibility in y-direction."""
+        self._yflow = (self.rrock._yperm*self._yarea)/(self._ydelta)
+
+    @property
+    def zflow(self):
+        """Getter for the rock transmissibility in z-direction."""
+        return self._zflow/(9.869233e-16*0.3048)
+
+    @zflow.setter
+    def zflow(self,value):
+        """Setter for the rock transmissibility in z-direction."""
+        self._zflow = (self.rrock._zperm*self._zarea)/(self._zdelta)
+
+    @property
+    def fluid(self):
+        """Getter for the reservoir fluid properties."""
+        return self._fluid
+    
+    @fluid.setter
+    def fluid(self,value):
+        """Sets fluid potential and mobility into the cell."""
+        if value is None:
+            return
+
+        self._fluid = value
+        
+        # Reset fluid properties to indicate they need recalculation
+        self.hhead = None # Hydrostatic head
+        self.power = None # Fluid Potential (hydrostatic head + fluid pressure)
+        self.mobil = None # Fluid Mobility
+
+    @property
+    def hhead(self):
+        """Getter for the hydrostatic head of grids in psi."""
+        return self._hhead/6894.76
+
+    @hhead.setter
+    def hhead(self,value):
+        """Setter for the fluid's hydrostatic head in Pa."""
+        self._hhead = self.fluid._grad*self._depth
+
+    @property
+    def power(self):
+        """Getter for the fluid's phase potential at grids in psi."""
+        return self._power/6894.76
+    
+    @power.setter
+    def power(self,value):
+        """Setter for the fluid's phase potential at grids in Pa."""
+        self._power = self.fluid._press+self.fluid._grad*self._depth
+
+    @property
+    def mobil(self):
+        """Getter for the fluid's phase mobility."""
+        return self._mobil*6894.76*(24*60*60)
+    
+    @mobil.setter
+    def mobil(self,value):
+        """Setter for the fluid's phase mobility."""
+        self._mobil = (self.fluid._rperm)/(self.fluid._visc*self.fluid._fvf)
+
+    @property
+    def tcomp(self):
+        """Getter for the total compressibility."""
+        return self._tcomp*6894.76
+
+    @tcomp.setter
+    def tcomp(self,value):
+        """Setter for the total compressibility."""
+        self._tcomp = self.rrock._comp+self.fluid._comp if value is None else value/6894.76
+
+if __name__ == "__main__":
+
+	pass
